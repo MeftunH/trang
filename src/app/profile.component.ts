336120ddebba5201874227745a46da9c52f4eecd
MeftunH <maftunhashimli@gmail.com>
2024-03-17 02:45:38 +0300

    add profile component

---
+++ src/app/profile.component.ts
@@ -0,0 +1,27 @@
+ import { Component}  from "@angular/core";
+
+
+ @Component({
+  selector: 'app-profile',
+  template: "<h1>Dymamic Profile Template</h1>",
+  styles: [
+    `
+      :host {
+        display: block;
+        padding: 10px;
+        border: 1px solid #ccc;
+        margin: 10px;
+        background-color: #f1f1f1;
+        color: #333;
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+      }
+    `
+  ]
+
+})
+
+export class ProfileComponent{
+  title = 'trang';
+}
