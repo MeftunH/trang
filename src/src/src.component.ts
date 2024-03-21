11c54146476e3435f043903bf0ecd72a59b01555
MAFTUN <maftun.hashimli@assistt.com.tr>
2024-03-21 23:57:32 +0300

    Refactor code to improve performance and readability

---
+++ src/src/src.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-src',
+  standalone: true,
+  imports: [],
+  templateUrl: './src.component.html',
+  styleUrl: './src.component.css'
+})
+export class SrcComponent {
+
+}
